4e745532b0ad9bb12d894809a5b88eda5e4d9cd0
Lian08 <lian080408@gmail.com>
2026-06-08 08:43:42 +0900

    chore: PropertiesConfiguration 제거

--- src/main/java/kr/magicbox/search/global/configuration/PropertiesConfiguration.java
+++
@@ -1,9 +0,0 @@
-package kr.magicbox.search.global.configuration;
-
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ConfigurationPropertiesScan(basePackages = "kr.magicbox.search")
-public class PropertiesConfiguration {
-}
